178a9657a3aeb41d557e5e6e54a4acfedea00f9d
Mark Stoumann <markstoumann@Marks-Laptop.lan>
2023-05-15 12:14:14 +0200

    Overflow dude

--- components/section/Garage.vue
+++ components/section/Garage.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="w-full h-[980px] bg-secondary text-body-dark flex items-center justify-end">
+    <section class="w-full h-[980px] bg-secondary text-body-dark flex items-center justify-end overflow-hidden">
         <div class="translate-x-1/4" v-parallax:x="{percentMax: 50, percentMin: -30, direction: 'left'}">
             <h3 class="text-body-dark">introducing our</h3>
             <h1 class="leading-none -mt-[1vw]">
