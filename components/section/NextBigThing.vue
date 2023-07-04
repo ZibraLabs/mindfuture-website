0f6d25cb5bda381417adcd3bbd2a410b433a90f9
Mark Stoumann <markstoumann@Marks-Laptop.lan>
2023-07-04 14:04:41 +0200

    Made line thinner

--- components/section/NextBigThing.vue
+++ components/section/NextBigThing.vue
@@ -9,7 +9,7 @@
             <div class="relative">
                 <div class="absolute inset-0 bg-gradient-to-b from-body-dark to-transparent"></div>
                 <svg stroke="currentColor" class="h-[22vw]" viewBox="0 0 229 498" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path class="moving-line" stroke-width="3" d="M220.65,0v208.91c0,22.09-17.91,40-40,40H40c-22.09,0-40,17.91-40,40v203.48" />
+                    <path class="moving-line" stroke-width="2" d="M220.65,0v208.91c0,22.09-17.91,40-40,40H40c-22.09,0-40,17.91-40,40v203.48" />
                 </svg>
             </div>
         </div>
