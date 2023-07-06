6f176f9ca4ec4034833cee7e5dfca8514bcfc689
Mark Stoumann <mark@stoumann.dk>
2023-07-06 12:42:40 +0200

    New video

--- components/section/HeroVideo.vue
+++ components/section/HeroVideo.vue
@@ -1,5 +1,5 @@
 <template>
     <section class="h-[60vh] lg:h-[40vw] w-full relative">
-        <video class="fullscreen" src="@/assets/video/2.mp4" autoplay loop muted></video>
+        <video class="fullscreen" src="@/assets/video/dronevideo.mp4" autoplay loop muted></video>
     </section>
 </template>
