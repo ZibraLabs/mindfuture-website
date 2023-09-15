c78e094c509bbf3b23f4df2f16d31256038b1576
Mark Stoumann <mark@stoumann.dk>
2023-09-15 14:13:28 +0200

    Pillars change

--- components/section/Pillars.vue
+++ components/section/Pillars.vue
@@ -7,9 +7,26 @@
             <div class="pillar-box bg-body-dark overflow-hidden flex items-center justify-center w-[1vw] h-[0.6vh] desktop-sm:w-[40vw] desktop-sm:h-[40vh]"></div>
             <div class="pillar-text absolute inset-0 flex items-center desktop-sm:justify-center pl-10 desktop-sm:pl-4 p-4 desktop-sm:p-0 z-10 opacity-0">
                 <div class="space-y-12 max-w-[40vw] relative">
-                    <h3 class="text-body-gray-light desktop-sm:!text-primary">Mindfuture pillars</h3>
-                    <h2>3 steps towards something great</h2>
-                    <div class="absolute -bottom-12 border-b border-border left-0 w-screen h-px hidden desktop-sm:block"></div>
+                    <!-- <h3 class="text-body-gray-light desktop-sm:!text-primary">Mindfuture pillars</h3>
+                    <h2>3 steps towards something great</h2> -->
+                    <div class="-translate-x-full">
+                        <div class="text-scroll-fast">
+                            <h1 class="leading-none text-body-gray-dark">Pillars</h1>
+                            <h1 class="leading-none text-primary">Pillars</h1>
+                            <h1 class="leading-none text-body-gray-dark">Pillars</h1>
+                            <h1 class="leading-none text-primary">Pillars</h1>
+                            <h1 class="leading-none text-body-gray-dark">Pillars</h1>
+                            <h1 class="leading-none text-primary">Pillars</h1>
+                            <h1 class="leading-none text-body-gray-dark">Pillars</h1>
+                            <h1 class="leading-none text-primary">Pillars</h1>
+                            <h1 class="leading-none text-body-gray-dark">Pillars</h1>
+                            <h1 class="leading-none text-primary">Pillars</h1>
+                        </div>
+                    </div>
+                    <div class="border-b border-border left-0 w-screen h-px hidden desktop-sm:block"></div>
+                    <div class="flex items-baseline space-x-2">
+                        <h4 class="text-body-gray-dark">3 pillars</h4> <h4 class="text-body-light">towards something great</h4>
+                    </div>
                 </div>
             </div>
         </div>
