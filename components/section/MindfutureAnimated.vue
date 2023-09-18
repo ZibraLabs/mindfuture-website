d7354d4c915cbf20ab8c66dcf4310dd499b6001c
Mark Stoumann <mark@stoumann.dk>
2023-09-18 13:54:25 +0200

    New video format

--- components/section/MindfutureAnimated.vue
+++ components/section/MindfutureAnimated.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="desktop-sm:flex desktop-sm:items-center desktop-sm:justify-start w-full desktop-sm:space-x-12 space-y-4 desktop-sm:space-y-0">
             <div>
-                <video src="@/assets/video/mind.mov" autoplay loop muted class="desktop-sm:h-[11vw]"></video>
+                <video src="@/assets/video/Mind.mp4" autoplay loop muted class="desktop-sm:h-[11vw]"></video>
             </div>
             <div>
                 <img src="@/assets/images/futuretext.svg" class="desktop-sm:h-[11vw]" />
