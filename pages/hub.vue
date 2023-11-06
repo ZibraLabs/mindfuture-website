fd4943e2db4904b4dc5dd19a6c3aed7693296d08
Mark Stoumann <mark@stoumann.dk>
2023-11-06 15:29:40 +0100

    New content

--- pages/hub.vue
+++ pages/hub.vue
@@ -3,6 +3,7 @@
         <div class="bg-section-background p-12">
             <div class="text-center leading-tight">
                 <h2>MF <span class="text-secondary">HUB</span></h2>
+                <p>Erik Husfeldts Vej 7, 2630 Taastrup</p>
             </div>
         </div>
         <div class="relative bg-body-gray-dark w-full aspect-video">
@@ -50,22 +51,97 @@
                     <h4 class="uppercase text-secondary">Join us in the<br class="block desktop-sm:hidden" /> MindFuture Hub</h4>
                     <h3 class="mt-12 text-body-gray-dark">Workspace</h3>
                     <div class="flex items-baseline space-x-4"><h2 class="text-primary">2.900,- <h5 class="text-body-light">DKK + VAT/mth</h5></h2></div>
-                    <h5 class="mt-6">Contact</h5>
-                    <p>
-                        Henriette Lundqvist<br />
-                        <div class="flex flex-col desktop-sm:flex-row desktop-sm:items-center desktop-sm:space-x-4">
-                            <a href="mailto:hll@mindfuture.com">hll@mindfuture.com</a>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="text-secondary h-8 w-8 hidden desktop-sm:block" viewBox="0 0 24 24"><path fill="currentColor" d="m6.05 19l5-7l-5-7H8.5l5 7l-5 7H6.05ZM12 19l5-7l-5-7h2.45l5 7l-5 7H12Z"/></svg>
-                            <a href="tel:31192182">tlf: +45 31192182</a>
-                        </div>
+                    <h5 class="mt-6">Contact the HUB Manager</h5>
+                </div>
+            </div>
+        </div>
+        <a href="https://instagram.com/mindfuture_hub" target="_blank" class="block p-12 bg-secondary hover:bg-primary font-bold text-body-dark w-full">
+            <div class="flex items-center justify-center space-x-4">
+                <div>Check out our instagram</div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/></svg>
+            </div>
+        </a>
+        <div class="grid desktop-sm:grid-cols-2 grid-cols-1 gap-6 desktop-sm:gap-12">
+            <div class="border-2 border-border p-4 desktop-sm:p-12 aspect-square flex items-center justify-center">
+                <div class="space-y-4 desktop-sm:space-y-8">
+                    <h4 class="uppercase text-secondary">Events / meetings <br />at Mindfuture Hub</h4>
+                    <ul class="space-y-4 desktop-sm:space-y-8">
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>State of the art <b>event facilities</b></div>
+                        </li>
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>Inspiring <b>meeting rooms</b></div>
+                        </li>
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>Complete <b>catering service</b></div>
+                        </li>
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>Unique <b>event planning</b></div>
+                        </li>
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>Free <b>parking</b> and <b>wi-fi</b></div>
+                        </li>
+                        <li class="flex items-center space-x-4">
+                            <div class="w-4 wide:w-[0.5vw] aspect-square bg-primary"></div>
+                            <div>Close to <b>freeway</b> and <b>public transportation</b></div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="bg-button-background p-4 desktop-sm:p-12 aspect-square flex items-center justify-center">
+                <div>
+                    <h4 class="uppercase text-secondary">Events / Meetings<br />at MindFuture Hub</h4>
+                    <h5 class="mt-6 text-body-light">Contact the HUB Manager for the price</h5>
+                </div>
+            </div>
+        </div>
+        <div class="bg-secondary p-12 text-body-dark">
+            <div class="grid grid-cols-2 desktop-sm:grid-cols-4 w-full gap-6 desktop-sm:gap-12">
+                <div>
+                    <img src="@/assets/images/hub/henriette.jpg" class="w-full aspect-square object-cover grayscale hover:grayscale-0 transition-all duration-500" />
+                    <p class="mt-4">
+                        <b class="uppercase">Henriette Lundqvist</b><br />
+                        <small><b>Hub Manager</b></small><br />
+                        <small><a href="mailto:hll@mindfuture.com">hll@mindfuture.com</a><br />
+                        <a href="tel:+4531192182">tlf: +45 31192182</a></small>
                     </p>
-                    <p>Erik Husfeldts Vej 7, 2630 Taastrup</p>
+                </div>
+                <div>
+                    <img src="@/assets/images/hub/jannick2.jpg" class="w-full aspect-square object-cover grayscale hover:grayscale-0 transition-all duration-500" />
+                    <p class="mt-4">
+                        <b class="uppercase">Jannick Uno Sørensen</b><br />
+                        <small><b>Facility Manager</b></small><br />
+                        <small><a href="mailto:hll@mindfuture.com">jus@mindfuture.com</a><br />
+                        <a href="tel:+4526166593">tlf: +45 26166593</a></small>
+                    </p>
+                </div>
+                <div>
+                    <img src="@/assets/images/hub/morten.jpg" class="w-full aspect-square object-cover grayscale hover:grayscale-0 transition-all duration-500" />
+                    <p class="mt-4">
+                        <b class="uppercase">Morten Hermansen</b><br />
+                        <small><b>Head Chef</b></small><br />
+                        <small><a href="mailto:hll@mindfuture.com">mh@mindfuture.com</a><br />
+                        <a href="tel:+4526790967">tlf: +45 26790967</a></small>
+                    </p>
+                </div>
+                <div>
+                    <img src="@/assets/images/hub/sona.jpg" class="w-full aspect-square object-cover grayscale hover:grayscale-0 transition-all duration-500" />
+                    <p class="mt-4">
+                        <b class="uppercase">Soňa Porvažník Lückstädt</b><br />
+                        <small><b>Kitchen Assistant</b></small><br />
+                        <small><a href="mailto:hll@mindfuture.com">spl@mindfuture.com</a><br />
+                        <a href="tel:+4542218973">tlf: +45 42218973</a></small>
+                    </p>
                 </div>
             </div>
         </div>
         <div class="grid grid-cols-3 w-full gap-6 desktop-sm:gap-12">
             <div @click="toggleScaleClass" class="cursor-zoom-in relative">
-                <!-- <img src="@/assets/images/hub/0.jpg" class="object-cover absolute z-50" /> -->
                 <img src="@/assets/images/hub/0.jpg" class="object-cover grayscale w-full h-full" />
             </div>
             <div @click="toggleScaleClass" class="cursor-zoom-in">
